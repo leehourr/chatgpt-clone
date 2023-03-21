cd5702a17556946e86b89ac262dfa2aac63ed59b
Lee Hour <lhour999@gmail.com>
2023-03-21 15:12:17 +0700

    ...

--- src/components/ChatArea/index.js
+++ src/components/ChatArea/index.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import HomePage from "./HomePage";
 import ChatArea from "./ChatArea";
 import sendIcon from "../../assets/send_icon.png";
 
 const Index = () => {
-  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef();
   const [messages, setMessages] = useState([]);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  //   console.log(messages);
+
+  const inputHandler = (e) => {
+    // console.log(e.code==="Enter");
+    const input = inputRef.current.value;
+    if (e.code === "Enter") {
+      console.log(input);
+      setMessages((prev) => [...prev, { input }]);
+    }
   };
 
   const handleFormSubmit = async (event) => {
@@ -26,16 +33,22 @@ const Index = () => {
   return (
     <div className="bg-[#343541] w-full h-full flex flex-col py-9 px-6 md:px-0 items-center justify-between ">
       <div className="flex-grow flex items-center justify-center">
-        {messages.length === 0 && <HomePage />}
-        {messages.length > 0 && <ChatArea />}
+        {messages?.length === 0 && <HomePage />}
+        {messages?.length > 0 && <ChatArea messages={messages} />}
       </div>
 
       <div className="relative w-full md:w-[32.5rem] shadow-lg shadow-black/10">
         <input
+          ref={inputRef}
+          onKeyDown={inputHandler}
           className="w-full  bg-[#40414F] py-2 rounded-md outline-none pl-3 text-[10px] "
           type="text"
         />
-        <img className="absolute invert opacity-50 cursor-pointer top-[0.35rem] hover:bg-white/90 right-2 w-5 p-1 rounded-md" src={sendIcon} alt="" />
+        <img
+          className="absolute invert opacity-50 cursor-pointer top-[0.35rem] hover:bg-white/90 right-2 w-5 p-1 rounded-md"
+          src={sendIcon}
+          alt=""
+        />
       </div>
     </div>
   );
