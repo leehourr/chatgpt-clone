2dc72ab9ec1c2d2b7b912361c7d9271ea88a24b9
Lee Hour <lhour999@gmail.com>
2023-03-22 05:26:48 +0700

    finished ChatArea now working on responsivness

--- src/components/ChatArea/ChatArea.js
+++ src/components/ChatArea/ChatArea.js
@@ -2,37 +2,50 @@ import React, { useContext, useMemo, useState } from "react";
 import user from "../../assets/user.jpg";
 import chatGpt from "../../assets/chatgpt-icon.png";
 import ChatContext from "../../context/Chat-context";
+import sinking from "../../assets/sinking.png";
 
-const Index = () => {
+const Index = ({ isLoading }) => {
   const chatCtx = useContext(ChatContext);
   const [messages, setMessages] = useState([]);
   useMemo(() => {
     setMessages((prev) => [...prev, ...chatCtx.chat]);
   }, [chatCtx]);
-  console.log(messages);
+  console.log(isLoading);
   return (
     <main className="flex flex-col justify-start w-full h-full">
       {messages.map((i, index) =>
         i.sender === "user" ? (
-          <>
-            <section
-              key={index}
-              className="w-full md:w-[45%] flex items-center justify-start gap-[13px] mx-auto py-4 "
-            >
-              <img className="w-6 h-6 rounded-sm" src={user} alt="" />
-
-              <p>{i.text}</p>
-            </section>
-          </>
+          <section
+            key={index}
+            className="w-full px-6 md:px-0 md:w-[45%] flex items-center justify-start gap-[13px] mx-auto py-4 "
+          >
+            <img className="w-6 h-6 rounded-sm" src={user} alt="" />
+            <p>{i.text}</p>
+          </section>
         ) : (
-          <section key={index} className="w-full bg-[#444654]">
+          <section
+            key={index + Math.random()}
+            className="w-full px-6 md:px-0 bg-[#444654]"
+          >
             <div className="w-full md:w-[45%] flex items-center justify-start gap-[13px] mx-auto py-4">
               <img className="w-6 h-6 rounded-sm" src={chatGpt} alt="" />
-
               <p>{i.text}</p>
             </div>
           </section>
         )
+      )}
+      {isLoading && (
+        <section className="w-full px-6 md:px-0 bg-[#444654]">
+          <div className="w-full md:w-[45%] flex items-start justify-start mx-auto py-4">
+            <img className="w-6 h-6 rounded-sm" src={chatGpt} alt="" />
+            <img
+              className="w-16 h-16 sm:w-28 sm:h-28 ml-7"
+              src={sinking}
+              alt=""
+            />
+            <h1 className="text-lg sm:text-xl font-semibold">Sinking...</h1>
+          </div>
+        </section>
       )}
     </main>
   );
--- src/components/ChatArea/index.js
+++ src/components/ChatArea/index.js
@@ -8,51 +8,53 @@ import { askChatGpt } from "../../utils/api";
 const Index = () => {
   const inputRef = useRef("");
   const chatCtx = useContext(ChatContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   const submitHandler = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
+    if (isLoading) return;
     const input = inputRef.current.value;
     if (input.trim() !== "") {
       chatCtx.addChatHandler({ sender: "user", text: input });
-      inputRef.current.value = "";
       const res = await askChatGpt(input);
-      console.log(res);
+      // console.log(res);
       chatCtx.addChatHandler({
         sender: "chatgpt",
         text: res.data.choices[0].message.content,
       });
-      return;
     }
+    setIsLoading(false);
     inputRef.current.focus();
-    return;
   };
 
   return (
-    <div className="bg-[#343541] w-full h-full flex flex-col pb-8 px-6 md:px-0 items-center justify-between">
+    <div className="bg-[#343541] w-full h-full flex flex-col pb-8 items-center justify-between">
       <header className="flex items-center justify-between mx-2"></header>
-      <div className="flex-grow flex items-center justify-center w-full overflow-y-auto">
+      <section className="flex-grow flex items-center justify-center w-full overflow-y-auto">
         {chatCtx.newChat && <HomePage />}
-        {!chatCtx.newChat && <ChatArea />}
-      </div>
-
-      <form
-        onSubmit={submitHandler}
-        className="relative w-full md:w-[32.5rem] shadow-lg shadow-black/10"
-      >
-        <input
-          ref={inputRef}
-          defaultValue=""
-          className="w-full  bg-[#40414F] py-2 rounded-md outline-none pl-3 text-[10px] "
-          type="text"
-        />
-        <button type="submit">
-          <img
-            className="absolute invert opacity-50 cursor-pointer top-[0.35rem] hover:bg-white/90 right-2 w-5 p-1 rounded-md"
-            src={sendIcon}
-            alt=""
+        {!chatCtx.newChat && <ChatArea isLoading={isLoading} />}
+      </section>
+      <section className="w-full px-6 md:w-[32.5rem] ">
+        <form
+          onSubmit={submitHandler}
+          className="relative w-fullshadow-lg shadow-black/10"
+        >
+          <input
+            ref={inputRef}
+            defaultValue=""
+            className="w-full  bg-[#40414F] py-2 rounded-md outline-none pl-3 text-[10px] "
+            type="text"
           />
-        </button>
-      </form>
+          <button type="submit">
+            <img
+              className="absolute invert opacity-50 cursor-pointer top-[0.35rem] hover:bg-white/90 right-2 w-5 p-1 rounded-md"
+              src={sendIcon}
+              alt=""
+            />
+          </button>
+        </form>
+      </section>
     </div>
   );
 };
